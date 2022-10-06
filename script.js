06793eccf2a069e724251a723014f9408e5fc5fc
bekfast34 <109563721+bekfast34@users.noreply.github.com>
2022-10-06 09:30:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -64,7 +64,37 @@ holding a body with a constraint pushes on other bodies too easily
   mostly fixed by capping the mass of what player can hold
 
 */
+var Engine = Matter.Engine,
+    Render = Matter.Render,
+    Runner = Matter.Runner,
+    Bodies = Matter.Bodies,
+    Composite = Matter.Composite;
 
+// create an engine
+var engine = Engine.create();
+
+// create a renderer
+var render = Render.create({
+    element: document.body,
+    engine: engine
+});
+
+// create two boxes and a ground
+var boxA = Bodies.rectangle(400, 200, 80, 80);
+var boxB = Bodies.rectangle(450, 50, 80, 80);
+var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+
+// add all of the bodies to the world
+Composite.add(engine.world, [boxA, boxB, ground]);
+
+// run the renderer
+Render.run(render);
+
+// create runner
+var runner = Runner.create();
+
+// run the engine
+Runner.run(runner, engine);
 //set up canvas
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext("2d");
